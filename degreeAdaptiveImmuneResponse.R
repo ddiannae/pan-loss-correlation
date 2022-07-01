41c803b03ce7ae047907407a1e458732849536f2
Diana Elisa García Cortés dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2022-07-01 17:01:36 -0500

    Adding scripts to plot degree and coreness community distribution

---
+++ degreeAdaptiveImmuneResponse.R
@@ -0,0 +1,80 @@
+########################################################################
+## Script to generate degree distribution of the  comms to comms 
+## network with a highlight of the GO:0002250 communities. 
+######################################################################
+
+library(dplyr)
+library(readr)
+library(igraph)
+library(ggplot2)
+library(ggthemes)
+library(scales)
+
+getDegreesDistribution <-  function(cond) {
+  tissues <- c("bladder", "brain", "breast", "colorectal", "esophagus",
+               "kidney", "liver", "lung", "ovary", "pancreas", "prostate",
+               "testis", "thyroid","skin", "uterus")
+  
+  all_degrees <- lapply(tissues, function(tissue) {
+    interactions <- read_tsv(paste0(tissue, "/network_aracne/communities/", cond, "-comm-network-all-louvain-100000.tsv")) %>%
+      rename("weight" = "n")
+    net <- igraph::graph_from_data_frame(interactions, directed = FALSE)
+    net <- igraph::simplify(net)
+    degree <- igraph::degree(net)
+    tibble(community = names(degree), degree = unname(degree), tissue = tissue)
+  })
+  
+  all_degrees <- bind_rows(all_degrees)
+  all_degrees <- all_degrees %>% 
+    filter(degree > 0)
+  
+  adaptive_enrich <- read_tsv(paste0("pan-loss/enrichments/comm-enrich-", cond, "_10.tsv")) %>%
+    filter(id == "GO:0002250") %>% 
+    select(id = comm_tissue)
+  all_degrees %>%
+    mutate(adaptive_degree = if_else(community %in% adaptive_enrich$id, degree, NA_real_))
+}
+
+degrees_cancer <- getDegreesDistribution("cancer")
+degrees_normal <- getDegreesDistribution("normal")
+
+capitalize <- function(string) {
+  substr(string, 1, 1) <- toupper(substr(string, 1, 1))
+  string
+}
+
+p <- ggplot(degrees_cancer) +
+  geom_boxplot(aes(y = degree, x = tissue)) +
+  geom_hline(aes(yintercept = adaptive_degree, linetype = "GO:0002250. Adaptive immune response"),
+             color = "orange", size = 1) +
+  facet_wrap(~tissue, scales="free", labeller = labeller(tissue = capitalize), nrow = 3) +
+  theme_base(base_size = 25) +
+  ylab("Community Degree") +
+  xlab("") +
+  theme(axis.text.x = element_blank(), legend.title=element_blank(), 
+        axis.ticks.x=element_blank(),  legend.position="bottom",
+        plot.margin = unit(c(0.5, 1, 0.5, 0.5), "cm")) +
+  scale_y_continuous(labels = scales::label_number(accuracy = 1)) 
+
+png(paste0("pan-loss/network_aracne_plots/cancer_comms_comms_adaptive_immune.png"), 
+    width = 850, height = 650)
+print(p)
+dev.off()
+
+p <- ggplot(degrees_normal) +
+  geom_boxplot(aes(y = degree, x = tissue)) +
+  geom_hline(aes(yintercept = adaptive_degree, linetype = "GO:0002250. Adaptive immune response"),
+             color = "orange", size = 1) +
+  facet_wrap(~tissue, scales="free", labeller = labeller(tissue = capitalize), nrow = 3) +
+  theme_base(base_size = 25) +
+  ylab("Community Degree") +
+  xlab("") +
+  theme(axis.text.x = element_blank(), legend.title=element_blank(), 
+        axis.ticks.x=element_blank(),  legend.position="bottom",
+        plot.margin = unit(c(0.5, 1, 0.5, 0.5), "cm")) +
+  scale_y_continuous(labels = scales::label_number(accuracy = 1)) 
+
+png(paste0("pan-loss/network_aracne_plots/normal_comms_comms_adaptive_immune.png"), 
+    width = 850, height = 650)
+print(p)
+dev.off()
---
+++ knessAdaptiveImmuneResponse.R
@@ -0,0 +1,89 @@
+library(igraph)
+library(vroom)
+
+getCommKnessDistribution <-  function(cond) {
+  tissues <- c("bladder", "brain", "breast", "colorectal", "esophagus",
+               "kidney", "liver", "lung", "ovary", "pancreas", "prostate",
+               "testis", "thyroid","skin", "uterus")
+  
+  all_comm_kness <- lapply(tissues, function(tissue) {
+    interactions <- vroom::vroom(paste0(tissue, "/network_aracne/", cond, "-interactions-100000.tsv"))
+    vertices <- vroom::vroom(paste0(tissue, "/network_aracne/", cond, "-vertices-100000.tsv"), 
+                             col_types = cols_only("ensembl" = col_character())) %>%
+      dplyr::inner_join(vroom::vroom(paste0(tissue, 
+                                            "/network_aracne/communities/", cond, "-comm-all-louvain-100000.tsv")),
+                        by = "ensembl")
+    comms_info <- vroom::vroom(paste0(tissue, "/network_aracne/communities/", cond, "-comm-info-all-louvain-100000.tsv"),
+                               col_types = cols_only("com_id" = col_double(), "order" = col_double()))
+    
+    net <- igraph::graph_from_data_frame(interactions, directed = F, vertices = vertices)
+    
+    vertices <- igraph::coreness(net) %>%
+      stack() %>%
+      dplyr::rename(kness = values, ensembl = ind) %>%
+      dplyr::inner_join(vertices, by = "ensembl")
+    
+    kness_comm <- vertices %>% 
+      dplyr::group_by(community) %>%
+      dplyr::summarise(mean_kness = mean(kness)) %>%
+      dplyr::inner_join(comms_info, by = c("community" = "com_id")) %>%
+      dplyr::filter(order > 5) %>%
+      dplyr::mutate(community = paste(tissue, community, sep = "_" ), 
+                    tissue = tissue)
+  
+  })
+  
+  all_comm_kness <- bind_rows(all_comm_kness)
+  
+  adaptive_enrich <- read_tsv(paste0("pan-loss/enrichments/comm-enrich-", cond, "_10.tsv")) %>%
+    filter(id == "GO:0002250") %>% 
+    select(id = comm_tissue)
+  
+  all_comm_kness %>%
+    mutate(adaptive_kness = if_else(community %in% adaptive_enrich$id, mean_kness, NA_real_))
+}
+
+
+kness_cancer <- getCommKnessDistribution("cancer")
+kness_normal <- getCommKnessDistribution("normal")
+
+capitalize <- function(string) {
+  substr(string, 1, 1) <- toupper(substr(string, 1, 1))
+  string
+}
+
+p <- ggplot(kness_cancer) +
+  geom_boxplot(aes(y = mean_kness, x = tissue)) +
+  geom_hline(aes(yintercept = adaptive_kness, linetype = "GO:0002250. Adaptive immune response"),
+             color = "orange", size = 1) +
+  facet_wrap(~tissue, scales="free", labeller = labeller(tissue = capitalize), nrow = 3) +
+  theme_base(base_size = 25) +
+  ylab("Mean Community Coreness") +
+  xlab("") +
+  theme(axis.text.x = element_blank(), legend.title=element_blank(), 
+        axis.ticks.x=element_blank(),  legend.position="bottom",
+        plot.margin = unit(c(0.5, 1, 0.5, 0.5), "cm")) +
+  scale_y_continuous(labels = scales::label_number(accuracy = 1)) 
+
+png(paste0("pan-loss/network_aracne_plots/cancer_kness_adaptive_immune.png"), 
+    width = 850, height = 650)
+print(p)
+dev.off()
+
+p <- ggplot(kness_normal) +
+  geom_boxplot(aes(y = mean_kness, x = tissue)) +
+  geom_hline(aes(yintercept = adaptive_kness, linetype = "GO:0002250. Adaptive immune response"),
+             color = "orange", size = 1) +
+  facet_wrap(~tissue, scales="free", labeller = labeller(tissue = capitalize), nrow = 3) +
+  theme_base(base_size = 25) +
+  ylab("Mean Community Coreness") +
+  xlab("") +
+  theme(axis.text.x = element_blank(), legend.title=element_blank(), 
+        axis.ticks.x=element_blank(),  legend.position="bottom",
+        plot.margin = unit(c(0.5, 1, 0.5, 0.5), "cm")) +
+  scale_y_continuous(labels = scales::label_number(accuracy = 1)) 
+
+png(paste0("pan-loss/network_aracne_plots/normal_kness_adaptive_immune.png"), 
+    width = 850, height = 650)
+print(p)
+dev.off()
